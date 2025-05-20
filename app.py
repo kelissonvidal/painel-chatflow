09d1d1ef95f8cecc98041c47d28c2b79eb7ecdcd
kelissonvidal <115296709+kelissonvidal@users.noreply.github.com>
2025-05-20 15:43:09 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
-placeholder app
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/login")
+def login():
+    return render_template("form.html")
+
+@app.route("/audios")
+def audios():
+    return render_template("audios.html")
+
+@app.route("/ver_logs")
+def logs():
+    try:
+        with open("config.json", "r", encoding="utf-8") as f:
+            dados = json.load(f)
+        conteudo = json.dumps(dados, indent=2, ensure_ascii=False)
+    except:
+        conteudo = "Nenhuma configuração encontrada."
+    return render_template("logs.html", logs=conteudo)
+
+@app.route("/salvar", methods=["POST"])
+def salvar():
+    dados = {
+        "saudacao": request.form["saudacao"],
+        "coleta_nome": request.form["coleta_nome"],
+        "resposta_nome": request.form["resposta_nome"],
+        "pergunta_interesse": request.form["pergunta_interesse"],
+        "pergunta_pagamento": request.form["pergunta_pagamento"],
+        "pergunta_forma": request.form["pergunta_forma"],
+        "pergunta_info": request.form["pergunta_info"],
+        "encerramento": request.form["encerramento"]
+    }
+    with open("config.json", "w", encoding="utf-8") as f:
+        json.dump(dados, f, indent=2, ensure_ascii=False)
+    return render_template("salvo.html")
